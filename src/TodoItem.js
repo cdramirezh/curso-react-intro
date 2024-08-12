85e3d896f02d8b96b03cfd175bf947b3d8741c8c
higiniecito <cdramirezh@unal.edu.co>
2024-08-12 18:29:12 -0500

    Fix accessibility issue

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,11 @@ import "./TodoItem.css";
 export const TodoItem = props => {
 	return (
 		<li className={`list-group-item todo__item ${props.completed ? "todo__item--active" : ""}`}>
-			<button type="button" className="todo__check">
+			<button type="button" className="todo__check" title="Check">
 				<i className="bi bi-check-lg"></i>
 			</button>
 			<span>Llorar con la Llorona</span>
-			<button type="button" className="btn-close" aria-label="Close"></button>
+			<button type="button" className="btn-close" title="Delete"></button>
 		</li>
 	);
 };
