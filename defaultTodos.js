ffe9adc229eeeecf3f632e4ba67bfa909bd5f1dd
higiniecito <cdramirezh@unal.edu.co>
2024-09-10 11:04:23 -0500

    Updates

---
+++ defaultTodos.js
@@ -0,0 +1,7 @@
+export const defaultTodos = [
+	{ id: 1, text: "Cortar cebolla", completed: true },
+	{ id: 2, text: "Tomar leche", completed: true },
+	{ id: 3, text: "Llorar con la llorona", completed: false },
+	{ id: 4, text: "La la la", completed: false },
+	{ id: 5, text: "Usar estados", completed: true },
+];
